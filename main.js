c0cf238a4681b525731718d32ec89b04d800231a
Daniyal Ahmed <datariq0311@gmail.com>
2024-06-11 22:09:36 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+import inquirer from "inquirer";
+let todoList = [];
+let condition = true;
+console.log("\n\tWelcome to CodeWithDaniyal - ToDoList APPLICATION\n");
+while (condition) {
+    let userInput = await inquirer.prompt([
+        {
+            type: "input",
+            name: "task",
+            message: "Enter Your New Task",
+        }
+    ]);
+    todoList.push(userInput.task);
+    console.log(`${userInput.task} Task Added`);
+    let addMoreTask = await inquirer.prompt([
+        {
+            type: "confirm",
+            name: "addMoreTask",
+            message: "Do You Want To Add More Task?",
+            default: "False",
+        }
+    ]);
+    condition = addMoreTask.addmore;
+}
+console.log(`Your updated today list ${todoList}`);
